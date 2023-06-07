db1c62dd1a6b02e4ef5906719f1ea4bf3b3bdaf8
Rohith Abishua J <133746531+jrohitab@users.noreply.github.com>
2023-06-07 18:48:06 +0530

    Update 33) Ticket Charge.c

--- 33) Ticket Charge.c
+++ 33) Ticket Charge.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 
-double calculateTicketCharge(int age) {
-    double ticketCharge = 0.0;
-    
-    if (age >= 55) {
-        ticketCharge = 10.0;
-    } else if (age >= 21 && age <= 54) {
-        ticketCharge = 15.0;
-    } else if (age >= 13 && age <= 20) {
-        ticketCharge = 10.0;
-    } else if (age >= 3 && age <= 12) {
-        ticketCharge = 5.0;
-    } else {
-        ticketCharge = 0.0;
-    }
-    
-    return ticketCharge;
-}
-
 int main() {
     int age;
-    
+    double ticketCharge = 0.0;
+
     printf("Enter the age of the person: ");
     scanf("%d", &age);
-    
-    double ticketCharge = calculateTicketCharge(age);
-    
-    printf("Ticket Charge: $%.2lf\n", ticketCharge);
-    
+
+    if (age >= 55) {
+        ticketCharge = 10.00;
+    } else if (age >= 21 && age <= 54) {
+        ticketCharge = 15.00;
+    } else if (age >= 13 && age <= 20) {
+        ticketCharge = 10.00;
+    } else if (age >= 3 && age <= 12) {
+        ticketCharge = 5.00;
+    } else if (age < 3) {
+        ticketCharge = 0.00;
+    }
+
+    printf("Ticket charge: $%.2lf\n", ticketCharge);
+
     return 0;
 }
